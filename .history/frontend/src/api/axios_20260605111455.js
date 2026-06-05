a774b24ed117a08d6fd32c147544f43d3bba12e0
deepak445566 <deepakpandey1212d@gmail.com>
2026-06-05 11:15:01 +0530

    hjh

--- .history/frontend/src/api/axios_20260605111455.js
+++ .history/frontend/src/api/axios_20260605111455.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task1-q2f5.onrender.com/api",
+  baseURL: "https://task1-q2f5.onrender.com//api",
   withCredentials: true, 
 });
 
